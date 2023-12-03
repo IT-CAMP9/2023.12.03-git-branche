efee7068436d5e3af25ad43c279929841ccffe5a
Mateusz <bmatteo@interia.pl>
2023-12-03 09:45:19 +0100

    commit

--- src/pl/camp/it/git/App.java
+++ src/pl/camp/it/git/App.java
@@ -3,5 +3,7 @@ package pl.camp.it.git;
 public class App {
     public static void main(String[] args) {
         System.out.println("pierwsza linia !!");
+
+        System.out.println("Kolejna linia !");
     }
 }
